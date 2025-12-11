ed8bf9642d1ab514207046ad5c88e6614e5dcd5f
Ax M. <axel.macedo612@gmail.com>
2025-12-10 23:32:35 -0600

    resuelto problema de visualizacion local a ver si funciona con github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Invitacion-Arturo/",
+  base: mode === "production" ? "/Invitacion-Arturo/" : "/",
   server: {
     host: "::",
     port: 8080,
